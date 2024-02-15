387e87ec286bd396271e14ec9b1e725c2134e724
RONO <113999110+Titusrono@users.noreply.github.com>
2024-02-15 08:08:57 +0300

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 
 export const routes: Routes = [
-    {'path': 'home', component:HomeComponent},
+    {'path': '', component:HomeComponent},
     {'path': 'about', component:AboutComponent},
     {'path': 'services', component:ServicesComponent}
 ];
